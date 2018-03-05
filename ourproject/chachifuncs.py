f082010042ca71f067b94a7a1442f2495a3ee58d
Sarah Alamdari <salamda1@uw.edu>
2018-03-05 15:13:38 -0800

    added DASH function for plotting graph

--- ourproject/chachifuncs.py
+++ ourproject/chachifuncs.py
@@ -1,5 +1,4 @@
-#Roberts Notebook converted to Function 
-
+import dash_html_components as html
 import glob 
 import os 
 import pandas as pd
@@ -8,10 +7,10 @@ import requests
 def get_data(filepath): 
     """Imports all data in given path"""
     rootdir = filepath
-    file_list = [f for f in glob.glob(os.path.join(rootdir,'*.xlsx'))] #iterate through dir to get excel files 
-    
+    file_list = [f for f in glob.glob(os.path.join(rootdir,'*.xlsx'))] #iterate through dir to get xlsx files 
     d = {} #initiate dict for data storage
     count = 0
+    
     for file in file_list:
         count += 1
         name = os.path.split(file)[1].split('.')[0]
@@ -21,4 +20,17 @@ def get_data(filepath):
         print "adding file", (count, name)
     return d
 ### There are 23 files in the CS2 directory, so we should have 23 entries in the dictionary - add unit test for this, super EASY check 
+
+def generate_table(data, maxrows=10):
+    """Generates a table with Header and Body given a data set and number of rows to display in Dash format"""
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in data.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(data.iloc[i][col]) for col in data.columns
+        ]) for i in range(min(len(data), maxrows))]
+    )
+
 

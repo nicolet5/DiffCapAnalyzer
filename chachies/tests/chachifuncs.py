db0a533fabc386684886a30f493bc9550cb7e0c5
Nicole Thompson <nicolet5@uw.edu>
2018-03-11 22:33:50 -0700

    edited test_chachifuncs nlt

--- chachies/tests/chachifuncs.py
+++ chachies/tests/chachifuncs.py
@@ -1,4 +1,3 @@
-#sarah
 import glob 
 from math import isclose 
 import numpy as np
@@ -14,7 +13,7 @@ import scipy.signal
 ################################
 
 def get_all_data(rootdir, path_to_raw_data_folder):
-    '''Gets all raw data from the rootdir (ie 'data/') and specified folder (path_to_raw_data_folder), i.e. 'Source_Data' (which is within , and then 
+    '''Gets all raw data from the rootdir (ie 'data/') and specified folder (path_to_raw_data_folder), i.e. 'Source_Data' (which is within rootdir), and then 
     1. separates it into raw cycles and puts them in a folder (data/Separated_Cycles/)
     2. cleans those separated cycles and puts them in a folder (data/Clean_Separated_Cycles/)
     3. recombines the cleaned, separated cycles and saves those data sets in a folder (data/Clean_Whole_Sets/)
@@ -39,6 +38,9 @@ def get_all_data(rootdir, path_to_raw_data_folder):
 def load_sep_cycles(getdata_filepath, savedata_filepath):
     """Get data from a specified filepath, separates out data into cycles and saves those cycles as .xlsx files in specified filepath (must be an existing folder)"""
     dfdict = get_data(getdata_filepath)
+    assert type(dfdict) == dict
+    assert os.path.exists(getdata_filepath) == 1
+    assert os.path.exists(savedata_filepath) == 1 
     for key in dfdict:
         all_cycles_df = dfdict[key]
         cycle_dict = sep_cycles(all_cycles_df)
@@ -50,33 +52,51 @@ def load_sep_cycles(getdata_filepath, savedata_filepath):
 def clean_calc_sep_smooth(dataframe, windowlength, polyorder):
     """Takes one cycle dataframe, calculates dq/dv, cleans the data, separates out charge and discharge, and applies sav-golay filter. Returns two dataframes, one charge and one discharge.
     Windowlength and polyorder are for the sav-golay filter."""
+    assert type(dataframe) = pd.DataFrame 
+    assert 'Current(A)'and 'Voltage(V)' and 'dQ/dV' in dataframe.columns 
+
     df1 = calc_dv_dqdv(dataframe)
     raw_charge = df1[df1['Current(A)'] > 0]
     raw_charge = raw_charge.reset_index(drop = True)
+    #this separated out the charging data and put it in 'raw_charge'. Reset the index as well. 
+
     raw_discharge = df1[df1['Current(A)'] < 0]
     raw_discharge = raw_discharge.reset_index(drop = True)
+    #this separated out the discharging data and put it in 'raw_discharge'. Reset the index as well.
+
     clean_charge2 = drop_0_dv(raw_charge)
-    #clean_charge2 = clean_charge2.reset_index(drop = True)
-    #clean_charge2 = clean_charge2.drop(index = 0)
-    #want to delete first datapoint in charge row here instead?
     clean_discharge2 = drop_0_dv(raw_discharge)
+    #apply the drop_0_dv function to clean out the wonky data points, especially near the voltage corresponding to the end of a cycle.  
+
     clean_charge2 = clean_charge2.sort_values(['Voltage(V)'], ascending = True)
     clean_discharge2 = clean_discharge2.sort_values(['Voltage(V)'], ascending = False)
-    #clean_discharge2 = clean_discharge2[:-1]
+    #sorted the values because sometimes the data points are out of order, especially in the first cycle of a battery.  
+
     cleandf2 = clean_charge2.append(clean_discharge2, ignore_index = True)
-    #cleandf2 = cleandf2.sort_values(['Voltage(V)'], ascending = True)
     cleandf2 = cleandf2.reset_index(drop = True)
+    #appends the clean charge and discharge cycles to get a full set of clean data. Reset the index as well. 
+
     charge, discharge = sep_char_dis(cleandf2)
+    #apply the sep_char_dis function to cleandf2. This is necessary because the sep_char_dis function actually 
+    #assigns the values of dq/dv to use. The discharge cycles need dq/dv to be calculated based on the 
+    #discharge capacity, and the charge cycles need dq/dv to be calculated based on the charge capacity. 
+    #This structure could probably be improved since we are separating out charge/discharge, then recombining, then
+    #separating again, then recombining. 
+
     if len(discharge) > windowlength:
         smooth_discharge = my_savgolay(discharge, windowlength, polyorder)
     else:
         discharge['Smoothed_dQ/dV'] = discharge['dQ/dV']
         smooth_discharge = discharge
+    #this if statement is for when the datasets have less datapoints than the windowlength given to the sav_golay filter. 
+    #without this if statement, the sav_golay filter throws an error when given a dataset with too few points. This way, 
+    #we simply forego the smoothing function. 
     if len(charge) > windowlength: 
         smooth_charge = my_savgolay(charge, windowlength, polyorder)
     else:
         charge['Smoothed_dQ/dV'] = charge['dQ/dV']
         smooth_charge = charge   
+    #same as above, but for charging cycles. 
     return smooth_charge, smooth_discharge
 
 def get_clean_cycles(import_filepath, save_filepath): 
@@ -92,10 +112,8 @@ def get_clean_cycles(import_filepath, save_filepath):
         charge, discharge = clean_calc_sep_smooth(data, 9, 3)
         clean_data = discharge.append(charge, ignore_index = True)
         
-        #clean_data = clean_data.sort_values(['Voltage'], ascending = True)
         clean_data = clean_data.reset_index(drop = True)
-        #clean_data = my_savgolay(clean_data1, 21, 3)
-        #####################################################################################3
+       
         clean_cycle = {name : clean_data}
         d.update(clean_cycle)
        # print("adding file to dictionary" + str(count) + ' ' + str(name))
@@ -166,9 +184,7 @@ def get_data(filepath):
         data = pd.read_excel(file,1)
         new_set = {name : data}
         d.update(new_set)
-       # print("adding file " + str(count) + ' ' + str(name))
     return d
-### ADD UNIT TEST:There are 23 files in the CS2 directory, so we should have 23 entries in the dictionary - add unit test for this, super EASY check 
 
 #separate out dataframes into cycles
 def sep_cycles(dataframe):
@@ -194,6 +210,11 @@ def save_sep_cycles_xlsx(cycle_dict, battname, path_to_folder):
 
 def calc_dv_dqdv(cycle_df):
     """This function calculates the dv and the dq/dv for a dataframe."""
+    assert type(cycle_df) == pd.DataFrame
+    assert 'Voltage(V)' in cycle_df.columns
+    assert 'Discharge_Capacity(Ah)' in cycle_df.columns 
+    assert 'Charge_Capacity(Ah)' in cycle_df.columns
+
     cycle_df = cycle_df.reset_index(drop = True)
     cycle_df['dV'] = None 
     cycle_df['Discharge_dQ'] = None
@@ -207,21 +228,20 @@ def calc_dv_dqdv(cycle_df):
     #calculate dq/dv based off of discharge capacity - might change this later so user can choose to use charge or discharge cap. 
     cycle_df['Discharge_dQ/dV'] = cycle_df['Discharge_dQ']/cycle_df['dV']
     cycle_df['Charge_dQ/dV'] = cycle_df['Charge_dQ']/cycle_df['dV']
-    return cycle_df
+    return cycle_df 
 
 
 def drop_0_dv(cycle_df_dv): 
     '''Drop rows where dv=0 (or about 0) in a dataframe that has already had dv calculated. Then recalculate dv and calculate dq/dv'''
     #this will clean up the data points around V = 4.2V (since they are holding it at 4.2V for a while).
+    assert 'dV' in cycle_df_dv.columns
+    assert 'Current(A)' in cycle_df_dv.columns 
+
     cycle_df_dv = cycle_df_dv.reset_index(drop = True)
    
     cycle_df_dv['dv_close_to_zero'] = None
     
-
-
     for i in range(1, len(cycle_df_dv)):
-    	#if (cycle_df_dv.loc[i, ('dV/dt(V/s)')] == 0 or isclose(cycle_df_dv.loc[i, ('Current(A)')], 0, abs_tol = 10**-3) or isclose(cycle_df_dv.loc[i, ('Voltage(V)')], 4.2, abs_tol = 10**-3)):
-    	#	cycle_df_dv = cycle_df_dv.drop(index = i)
     	if isclose(cycle_df_dv.loc[i, ('Current(A)')], 0, abs_tol = 10**-3):
     		cycle_df_dv = cycle_df_dv.drop(index = i)
 
@@ -246,8 +266,7 @@ def drop_0_dv(cycle_df_dv):
         for i in range(1, len(cycle_df_dv)):
             if isclose(cycle_df_dv.loc[i, ('dV')], 0, abs_tol = 10**-3): 
                 cycle_df_dv = cycle_df_dv.drop(index = i)
-                #if i-1 in cycle_df_dv.index:
-                #	cycle_df_dv = cycle_df_dv.drop(index = i-1)
+
         cycle_df_dv = cycle_df_dv.reset_index(drop = True)
 
         separate_dis_char = np.where(np.diff(np.sign(cycle_df_dv['Current(A)'])))
@@ -280,8 +299,6 @@ def drop_0_dv(cycle_df_dv):
     cycle_df_dv = cycle_df_dv.dropna(subset=['Discharge_dQ/dV'])
     cycle_df_dv = cycle_df_dv.dropna(subset=['Charge_dQ/dV'])
     cycle_df_dv = cycle_df_dv.reset_index(drop = True)
-   # cycle_df_dv = cycle_df_dv[:-1]
-   
  
 
 
@@ -290,32 +307,36 @@ def drop_0_dv(cycle_df_dv):
 
 def sep_char_dis(df_dqdv):
     '''Takes a dataframe of one cycle with calculated dq/dv and separates into charge and discharge differential capacity curves'''
+    assert 'Charge_dQ/dV' in df_dqdv.columns
+    assert 'Discharge_dQ/dV' in df_dqdv.columns
     charge = df_dqdv[df_dqdv['Current(A)'] > 0]
     charge.is_copy = None
     charge = charge.reset_index(drop = True)
     charge['dQ/dV'] = charge['Charge_dQ/dV']
+
     for i in range(1, len(charge)):
         if charge.loc[i, ('dQ/dV')] == 0: 
             charge = charge.drop(index = i)
     charge = charge.reset_index(drop = True)
-    #charge = charge.iloc[1:]
-    charge = charge.reset_index(drop = True)
+
     discharge = df_dqdv[df_dqdv['Current(A)'] < 0] 
     discharge.is_copy = None 
     discharge['dQ/dV'] = discharge['Discharge_dQ/dV']
     discharge = discharge.reset_index(drop = True)
+
     for i in range(1, len(discharge)):
         if discharge.loc[i, ('dQ/dV')] == 0: 
             discharge = discharge.drop(index = i)
-    discharge = discharge.reset_index(drop = True)
-    #discharge = discharge.iloc[:-1]
-    #discharge = discharge.iloc[2:]
     discharge = discharge.reset_index(drop = True)
     
     return charge, discharge
     
 def my_savgolay(dataframe, windowlength, polyorder):
     """Takes battery dataframe with a dQ/dV column and applies a sav_golay filter to it, returning the dataframe with a new column called Smoothed_dQ/dV"""
+    assert not windowlength % 2 == 0
+    #asserts this is an odd number - necessary to run the savgol_filter function 
+    assert polyorder < windowlength 
+    #necessary to run the savgol_filter function as well. 
     unfilt = pd.concat([dataframe['dQ/dV']])
     unfiltar = unfilt.values
     #converts into an array 

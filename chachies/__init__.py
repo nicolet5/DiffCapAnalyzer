e6bba808644abbafa60494f9d76caddd1617149f
Nicole Thompson <nicolet5@uw.edu>
2018-03-14 12:40:05 -0700

    edited __init__ files

--- chachies/__init__.py
+++ chachies/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 __all__ = ["chachifuncs","descriptors","chachies", "version"]
 
-from . import chachifuncs
-from .chachies import *
-from .descriptors import *
-from .version import __version__
+from chachies import chachifuncs
+from chachies import descriptors
+from chachies.version import __version__
+from chachies import chachies
